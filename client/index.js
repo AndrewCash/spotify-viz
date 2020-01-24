ea7004e40e804f16a46b9f88c887578d3adbb1d5
Andrew Cash <asc010@uark.edu>
2020-01-23 22:53:01 -0600

    Copied the example and moved some colors around

--- client/index.js
+++ client/index.js
@@ -1,10 +1,12 @@
 import { auth } from './classes/sync'
 import Template from './template'
 import Example from './example'
+import Custom from './custom1'
 
 if (window.location.hash === '#start') {
   // const template = new Template()
-  const example = new Example()
+  // const example = new Example()
+  const custom = new Custom()
 
   console.log("hello world")
 } else {
